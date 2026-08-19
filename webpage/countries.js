92d0b200aeba38a884a7ee2143cc8ed9bc9b198b
aghrabooti <parhamafzal@proton.me>
2026-08-19 16:29:58 +0330

    Create countries file

---
+++ webpage/countries.js
@@ -0,0 +1,548 @@
+const countries = {
+
+    AF: {
+        name: "Afghanistan",
+        flag: "🇦🇫",
+        lat: 33.9391,
+        lng: 67.7100
+    },
+
+    AL: {
+        name: "Albania",
+        flag: "🇦🇱",
+        lat: 41.1533,
+        lng: 20.1683
+    },
+
+    DZ: {
+        name: "Algeria",
+        flag: "🇩🇿",
+        lat: 28.0339,
+        lng: 1.6596
+    },
+
+    AR: {
+        name: "Argentina",
+        flag: "🇦🇷",
+        lat: -38.4161,
+        lng: -63.6167
+    },
+
+    AM: {
+        name: "Armenia",
+        flag: "🇦🇲",
+        lat: 40.0691,
+        lng: 45.0382
+    },
+
+    AU: {
+        name: "Australia",
+        flag: "🇦🇺",
+        lat: -25.2744,
+        lng: 133.7751
+    },
+
+    AT: {
+        name: "Austria",
+        flag: "🇦🇹",
+        lat: 47.5162,
+        lng: 14.5501
+    },
+
+    AZ: {
+        name: "Azerbaijan",
+        flag: "🇦🇿",
+        lat: 40.1431,
+        lng: 47.5769
+    },
+
+    BY: {
+        name: "Belarus",
+        flag: "🇧🇾",
+        lat: 53.7098,
+        lng: 27.9534
+    },
+
+    BE: {
+        name: "Belgium",
+        flag: "🇧🇪",
+        lat: 50.5039,
+        lng: 4.4699
+    },
+
+    BR: {
+        name: "Brazil",
+        flag: "🇧🇷",
+        lat: -14.2350,
+        lng: -51.9253
+    },
+
+    BG: {
+        name: "Bulgaria",
+        flag: "🇧🇬",
+        lat: 42.7339,
+        lng: 25.4858
+    },
+
+    CA: {
+        name: "Canada",
+        flag: "🇨🇦",
+        lat: 56.1304,
+        lng: -106.3468
+    },
+
+    CL: {
+        name: "Chile",
+        flag: "🇨🇱",
+        lat: -35.6751,
+        lng: -71.5430
+    },
+
+    CN: {
+        name: "China",
+        flag: "🇨🇳",
+        lat: 35.8617,
+        lng: 104.1954
+    },
+
+    CO: {
+        name: "Colombia",
+        flag: "🇨🇴",
+        lat: 4.5709,
+        lng: -74.2973
+    },
+
+    HR: {
+        name: "Croatia",
+        flag: "🇭🇷",
+        lat: 45.1000,
+        lng: 15.2000
+    },
+
+    CY: {
+        name: "Cyprus",
+        flag: "🇨🇾",
+        lat: 35.1264,
+        lng: 33.4299
+    },
+
+    CZ: {
+        name: "Czechia",
+        flag: "🇨🇿",
+        lat: 49.8175,
+        lng: 15.4730
+    },
+
+    DK: {
+        name: "Denmark",
+        flag: "🇩🇰",
+        lat: 56.2639,
+        lng: 9.5018
+    },
+
+    EG: {
+        name: "Egypt",
+        flag: "🇪🇬",
+        lat: 26.8206,
+        lng: 30.8025
+    },
+
+    EE: {
+        name: "Estonia",
+        flag: "🇪🇪",
+        lat: 58.5953,
+        lng: 25.0136
+    },
+
+    FI: {
+        name: "Finland",
+        flag: "🇫🇮",
+        lat: 61.9241,
+        lng: 25.7482
+    },
+
+    FR: {
+        name: "France",
+        flag: "🇫🇷",
+        lat: 46.2276,
+        lng: 2.2137
+    },
+
+    GE: {
+        name: "Georgia",
+        flag: "🇬🇪",
+        lat: 42.3154,
+        lng: 43.3569
+    },
+
+    DE: {
+        name: "Germany",
+        flag: "🇩🇪",
+        lat: 51.1657,
+        lng: 10.4515
+    },
+
+    GR: {
+        name: "Greece",
+        flag: "🇬🇷",
+        lat: 39.0742,
+        lng: 21.8243
+    },
+
+    HK: {
+        name: "Hong Kong",
+        flag: "🇭🇰",
+        lat: 22.3193,
+        lng: 114.1694
+    },
+
+    HU: {
+        name: "Hungary",
+        flag: "🇭🇺",
+        lat: 47.1625,
+        lng: 19.5033
+    },
+
+    IS: {
+        name: "Iceland",
+        flag: "🇮🇸",
+        lat: 64.9631,
+        lng: -19.0208
+    },
+
+    IN: {
+        name: "India",
+        flag: "🇮🇳",
+        lat: 20.5937,
+        lng: 78.9629
+    },
+
+    ID: {
+        name: "Indonesia",
+        flag: "🇮🇩",
+        lat: -0.7893,
+        lng: 113.9213
+    },
+
+    IR: {
+        name: "Iran",
+        flag: "🇮🇷",
+        lat: 32.4279,
+        lng: 53.6880
+    },
+
+    IQ: {
+        name: "Iraq",
+        flag: "🇮🇶",
+        lat: 33.2232,
+        lng: 43.6793
+    },
+
+    IE: {
+        name: "Ireland",
+        flag: "🇮🇪",
+        lat: 53.1424,
+        lng: -7.6921
+    },
+
+    IL: {
+        name: "Israel",
+        flag: "🇮🇱",
+        lat: 31.0461,
+        lng: 34.8516
+    },
+
+    IT: {
+        name: "Italy",
+        flag: "🇮🇹",
+        lat: 41.8719,
+        lng: 12.5674
+    },
+
+    JP: {
+        name: "Japan",
+        flag: "🇯🇵",
+        lat: 36.2048,
+        lng: 138.2529
+    },
+
+    KZ: {
+        name: "Kazakhstan",
+        flag: "🇰🇿",
+        lat: 48.0196,
+        lng: 66.9237
+    },
+
+    KR: {
+        name: "South Korea",
+        flag: "🇰🇷",
+        lat: 35.9078,
+        lng: 127.7669
+    },
+
+    LV: {
+        name: "Latvia",
+        flag: "🇱🇻",
+        lat: 56.8796,
+        lng: 24.6032
+    },
+
+    LT: {
+        name: "Lithuania",
+        flag: "🇱🇹",
+        lat: 55.1694,
+        lng: 23.8813
+    },
+
+    LU: {
+        name: "Luxembourg",
+        flag: "🇱🇺",
+        lat: 49.8153,
+        lng: 6.1296
+    },
+
+    MY: {
+        name: "Malaysia",
+        flag: "🇲🇾",
+        lat: 4.2105,
+        lng: 101.9758
+    },
+
+    MX: {
+        name: "Mexico",
+        flag: "🇲🇽",
+        lat: 23.6345,
+        lng: -102.5528
+    },
+
+    MD: {
+        name: "Moldova",
+        flag: "🇲🇩",
+        lat: 47.4116,
+        lng: 28.3699
+    },
+
+    MN: {
+        name: "Mongolia",
+        flag: "🇲🇳",
+        lat: 46.8625,
+        lng: 103.8467
+    },
+
+    ME: {
+        name: "Montenegro",
+        flag: "🇲🇪",
+        lat: 42.7087,
+        lng: 19.3744
+    },
+
+    MA: {
+        name: "Morocco",
+        flag: "🇲🇦",
+        lat: 31.7917,
+        lng: -7.0926
+    },
+
+    NP: {
+        name: "Nepal",
+        flag: "🇳🇵",
+        lat: 28.3949,
+        lng: 84.1240
+    },
+
+    NL: {
+        name: "Netherlands",
+        flag: "🇳🇱",
+        lat: 52.1326,
+        lng: 5.2913
+    },
+
+    NZ: {
+        name: "New Zealand",
+        flag: "🇳🇿",
+        lat: -40.9006,
+        lng: 174.8860
+    },
+
+    NO: {
+        name: "Norway",
+        flag: "🇳🇴",
+        lat: 60.4720,
+        lng: 8.4689
+    },
+
+    PK: {
+        name: "Pakistan",
+        flag: "🇵🇰",
+        lat: 30.3753,
+        lng: 69.3451
+    },
+
+    PA: {
+        name: "Panama",
+        flag: "🇵🇦",
+        lat: 8.5380,
+        lng: -80.7821
+    },
+
+    PE: {
+        name: "Peru",
+        flag: "🇵🇪",
+        lat: -9.1900,
+        lng: -75.0152
+    },
+
+    PH: {
+        name: "Philippines",
+        flag: "🇵🇭",
+        lat: 12.8797,
+        lng: 121.7740
+    },
+
+    PL: {
+        name: "Poland",
+        flag: "🇵🇱",
+        lat: 51.9194,
+        lng: 19.1451
+    },
+
+    PT: {
+        name: "Portugal",
+        flag: "🇵🇹",
+        lat: 39.3999,
+        lng: -8.2245
+    },
+
+    RO: {
+        name: "Romania",
+        flag: "🇷🇴",
+        lat: 45.9432,
+        lng: 24.9668
+    },
+
+    RU: {
+        name: "Russia",
+        flag: "🇷🇺",
+        lat: 61.5240,
+        lng: 105.3188
+    },
+
+    RS: {
+        name: "Serbia",
+        flag: "🇷🇸",
+        lat: 44.0165,
+        lng: 21.0059
+    },
+
+    SG: {
+        name: "Singapore",
+        flag: "🇸🇬",
+        lat: 1.3521,
+        lng: 103.8198
+    },
+
+    SK: {
+        name: "Slovakia",
+        flag: "🇸🇰",
+        lat: 48.6690,
+        lng: 19.6990
+    },
+
+    SI: {
+        name: "Slovenia",
+        flag: "🇸🇮",
+        lat: 46.1512,
+        lng: 14.9955
+    },
+
+    ZA: {
+        name: "South Africa",
+        flag: "🇿🇦",
+        lat: -30.5595,
+        lng: 22.9375
+    },
+
+    ES: {
+        name: "Spain",
+        flag: "🇪🇸",
+        lat: 40.4637,
+        lng: -3.7492
+    },
+
+    SE: {
+        name: "Sweden",
+        flag: "🇸🇪",
+        lat: 60.1282,
+        lng: 18.6435
+    },
+
+    CH: {
+        name: "Switzerland",
+        flag: "🇨🇭",
+        lat: 46.8182,
+        lng: 8.2275
+    },
+
+    TW: {
+        name: "Taiwan",
+        flag: "🇹🇼",
+        lat: 23.6978,
+        lng: 120.9605
+    },
+
+    TH: {
+        name: "Thailand",
+        flag: "🇹🇭",
+        lat: 15.8700,
+        lng: 100.9925
+    },
+
+    TR: {
+        name: "Turkey",
+        flag: "🇹🇷",
+        lat: 38.9637,
+        lng: 35.2433
+    },
+
+    UA: {
+        name: "Ukraine",
+        flag: "🇺🇦",
+        lat: 48.3794,
+        lng: 31.1656
+    },
+
+    AE: {
+        name: "United Arab Emirates",
+        flag: "🇦🇪",
+        lat: 23.4241,
+        lng: 53.8478
+    },
+
+    GB: {
+        name: "United Kingdom",
+        flag: "🇬🇧",
+        lat: 55.3781,
+        lng: -3.4360
+    },
+
+    US: {
+        name: "United States",
+        flag: "🇺🇸",
+        lat: 37.0902,
+        lng: -95.7129
+    },
+
+    UZ: {
+        name: "Uzbekistan",
+        flag: "🇺🇿",
+        lat: 41.3775,
+        lng: 64.5853
+    },
+
+    VN: {
+        name: "Vietnam",
+        flag: "🇻🇳",
+        lat: 14.0583,
+        lng: 108.2772
+    }
+};
